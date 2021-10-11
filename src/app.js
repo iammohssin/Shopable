a1960872976c353cbe3a959e51e011d28fb5e8fd
Mohsin Javaid <muhsanjavaid@gmail.com>
2021-10-11 12:57:07 +0500

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require('path');
 require("./db/conn");
+const User = require('./models/usermessage')
 const hbs = require("hbs");
 const Product = require('./models/products');
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,6 +14,7 @@ const partialpath = path.join(__dirname, "../templates/partials");
 app.use('/css', express.static(path.join(__dirname,"../node_modules/bootstrap/dist/css" )))
 app.use('/js', express.static(path.join(__dirname,"../node_modules/bootstrap/dist/js" )))
 app.use('/jq', express.static(path.join(__dirname,"../node_modules/jquery/dist" )))
+app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json());
 app.use(express.static(staticpath))
 app.set("view engine", "hbs");
@@ -45,7 +47,16 @@ app.post('/products', async (req, res) => {
    const product = await Product.create(req.body);
    res.json(product);
 });
-
+app.post("/contact",async (req,res) =>{
+    try{
+        // res.send(req.body)
+        const userData = new User(req.body);
+      await userData.save();
+      res.status(201).render("index");
+    }catch (error) {
+        res.status(500).send(error);
+    }
+})
 //server create
 app.listen(port,()=>{
     console.log(`server is running at port no ${port}`);
---
+++ src/models/usermessage.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required: true,
+        minLength:3
+    },
+    email:{
+        type: String,
+        required: true,
+         validate(value){
+           if(!validator.isEmail(value)){
+                throw new Error("Invalid email address")
+            }
+        }
+    },
+    phone:{
+        type:Number,
+        required:true,
+        min: 9
+    },
+    message:{
+        type:String,
+        required:true,
+        minLength: 5
+    }
+})
+
+const User = mongoose.model("User",userSchema);
+module.exports = User;
